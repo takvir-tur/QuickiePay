a98b42a897999d9b341c1e02e0bc11d0968c3d2f
yakeen04 <yakeen.9494@gmail.com>
2026-08-27 19:23:24 +0600

    neon.tech use

--- database/db_connection.js
+++ database/db_connection.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // Cloud database e SSL error avoid korar jonno
+  }
 });
 
 module.exports = pool;
